fbf7ddd754a8dfde23e6c6b51108f05dd41932a7
PieIsSpy <karldeejayomandac@gmail.com>
2026-01-11 12:22:29 +0800

    build: fixed redundant i/o paths, minor typo, and added md in template formats

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,14 @@ const htmlmin = require("html-minifier-terser");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
-    eleventyConfig.addPassthroughCopy("content/assets");
-    eleventyConfig.setTemplateFormats(["html", "njk", "txt", "js", "css", "xml", "json"]);
+    eleventyConfig.addPassthroughCopy("src/assets");
+    eleventyConfig.setTemplateFormats(["html", "njk", "txt", "js", "css", "xml", "json", "md"]);
     
     eleventyConfig.addTransform("htmlmin", function(content) {
         if ((this.page.outputPath || "").endsWith(".html")) {
             let minified = htmlmin.minify(content, {
                 collapseWhitespace: true,
-                conservativeCollaps: true,
+                conservativeCollapse: true,
                 collapseInlineTagWhitespace: true,
                 minifyCSS: true,
                 minifyJS: true,
