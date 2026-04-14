0300760466969aed8a1f3c07be3fe84a53eee670
PieIsSpy <karldeejayomandac@gmail.com>
2026-04-14 09:27:07 +0800

    feat: added data folder as global data

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,10 @@
 export default async function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/img');
     eleventyConfig.addPassthroughCopy('./src/styles');
+
     return {
         dir: {
+            data: '_data',
             input: 'src',
             output: 'public'
         }
